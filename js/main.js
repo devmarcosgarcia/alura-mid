47d5f225f09530134eed2dda7fc0175207402dc8
Marcos Quirino Garcia <marcosqgarcia@gmail.com>
2023-01-12 22:37:33 -0300

    final

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+   if(elemento != null && elemento.tagName === 'AUDIO'){
+    elemento.play();
+   }else{
+    console.log('Elemento não encontrado ou seletor inválido!');
+   }
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i < listaTeclas.length; i ++){
+
+    const tecla = listaTeclas[i]
+    const instrumento = listaTeclas[i].classList[1];
+    
+    const idAudio = `#som_${instrumento}`;
+
+    listaTeclas[i].onclick = function (){
+        
+         tocaSom(idAudio)
+    };
+
+    tecla.onkeydown = function (event) {
+
+        if(event.code === 'Space' || event.code === 'Enter'){
+
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
